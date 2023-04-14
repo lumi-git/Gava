04cbfcf5ec9924c6fc95dbbed35396ac5e8fa10b
ronantremoureux <ronan.tremoureux@gmail.com>
2023-04-14 20:11:48 +0200

    add support for github actions

---
+++ tests/Test_1.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class Test_1{
+
+    @Test
+    public void test_1() {
+        System.out.println("Hello world!");
+        assert "A".equals(Main.getA());
+    }
+}
